31516b9939ad2b99ddb5ce327531391e835df2d4
sojeongw <redthink90@google.com>
2019-05-26 21:24:47 +0900

    Spring Configuration with Java Annotations - Dependency Injection

--- spring-demo-annotation/src/com/luv2code/springdemo/AnnotationDemoApp.java
+++ spring-demo-annotation/src/com/luv2code/springdemo/AnnotationDemoApp.java
@@ -6,7 +6,8 @@ public class AnnotationDemoApp {
 
 	public static void main(String[] args) {
 
-		// read spring config file
+		// read spring config file: bean 등록 
+		// 이걸 불러옴으로써 TennisCoach에 있는 모든 생성자와 메소드가 실행된다.
 		ClassPathXmlApplicationContext context =
 				new ClassPathXmlApplicationContext("applicationContext.xml");
 		
@@ -17,9 +18,14 @@ public class AnnotationDemoApp {
 		// call a method on the bean
 		System.out.println(theCoach.getDailyWorkout());
 		
+		// call method to get the daily fortune
+		// autowired로 injection이 됐기 때문에 fortune에 있는 메소드를 coach에서 잘 불러온다.
+		// autowired를 없애면 NullPointerException이 뜬다.
+		System.out.println(theCoach.getDailyFortune());
+		
 		// close the context
 		context.close();
-		
+
 	}
 
 }
--- spring-demo-annotation/src/com/luv2code/springdemo/TennisCoach.java
+++ spring-demo-annotation/src/com/luv2code/springdemo/TennisCoach.java
@@ -1,15 +1,62 @@
 package com.luv2code.springdemo;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 // the bean id: Spring will automatically register this bean.
 @Component
 public class TennisCoach implements Coach {
 
+	// field injection
+	@Autowired
+	// default는 항상 첫 번째 lowercase이므로 따로 지정없이 사용한다.
+	@Qualifier("RESTFortuneService")
+	private FortuneService fortuneService;
+	
+	
+	// inject the properties values
+	@Value("${foo.email}")
+	private String email;
+	
+	
+	
+	// define a default constructor
+	public TennisCoach() {
+		System.out.println(">> TennisCoach: inside default constructor");
+	}
+	
+	/*
+	// define a setter method
+	// Autowired가 fortuneService를 implement 하는 클래스를 찾는다.
+	// 메소드 이름을 바꿔도 잘 불러온다!
+	@Autowired
+	public void doSomeCrazyStuff(FortuneService theFortuneService) {
+		System.out.println(">> TennisCoach: inside doSomeCrazyStuff() method");
+		fortuneService = theFortuneService;
+	}
+	*/
+	
+	/*
+	// Spring will scan for a component that implements FortuneService interface.
+	// HappyFortuneService meets the requirement
+	@Autowired
+	public TennisCoach(FortuneService theFortuneService) {
+		fortuneService = theFortuneService;
+	}
+	*/
+	
 	@Override
 	public String getDailyWorkout() {
-		
+		System.out.println("property value injection: " + email);
 		return "Practice your backhand volley";
+	}
+
+	@Override
+	public String getDailyFortune() {
+		
+		return fortuneService.getFortune();
 	}
 
 }
