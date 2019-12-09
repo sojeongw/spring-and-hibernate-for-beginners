2e35fc474a1e4628961a854e20981c7f3be529df
sojeongw <redthink90@gmail.com>
2019-12-10 06:44:14 +0900

    AOP: @before Advice Type 설명 추가

--- spring-demo-aop/src/com/luv2code/aopdemo/aspect/MyDemoLoggingAspect.java
+++ spring-demo-aop/src/com/luv2code/aopdemo/aspect/MyDemoLoggingAspect.java
@@ -4,8 +4,8 @@ import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
 import org.springframework.stereotype.Component;
 
-@Aspect
-@Component
+@Aspect	// aspect임을 알려줌
+@Component	// component scan으로 Aspect을 찾아냄
 public class MyDemoLoggingAspect {
 	
 	// this is where we add all of our related advises for logging
@@ -14,7 +14,7 @@ public class MyDemoLoggingAspect {
 	
 	/* fully qualified class name
 	 * 같은 method에 다른 parameter가 더 추가되면 동작하지 않는다.
-	@Before("execution(* add*(com.luv2code.aopdemo.Account))")
+	@Before("execution(* add*(com.luv2code.aopdemo.Account))")	// pointcut expression
 	*/
 	
 	/* must use fully qualified class name but let't get lazy
@@ -27,6 +27,7 @@ public class MyDemoLoggingAspect {
 	*/
 	
 	/* match on ANY parameters
+	 * add라는 이름이 들어가는 method면 다 match
 	@Before("execution(* add*(..))")
 	*/
 	
