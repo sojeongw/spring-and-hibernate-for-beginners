f1d527311c9789009e58077611a7eaf180265bcc
sojeongw <redthink90@gmail.com>
2019-12-23 10:47:56 +0900

    AOP: @After Advice Type

---
+++ spring-demo-aop-z-after-finally/src/com/luv2code/aopdemo/aspect/MyApiAnalyticsAspect.java
@@ -0,0 +1,17 @@
+package com.luv2code.aopdemo.aspect;
+
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.springframework.core.annotation.Order;
+import org.springframework.stereotype.Component;
+
+@Component
+@Aspect
+@Order(3)
+public class MyApiAnalyticsAspect {
+	@Before("com.luv2code.aopdemo.aspect.LuvAopExpressions.forDaoPackageNoGetterSetter()")
+	public void performApiAnalytics() {
+		System.out.println("=========>>> Performing API analytics");
+	}
+}
+
