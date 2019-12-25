fd15405da18197962ae5c7fef4587d25b1b23c80
sojeongw <redthink90@gmail.com>
2019-12-25 20:44:37 +0900

    Spring REST - Create a Spring REST Controller

--- spring-rest-demo/src/main/java/com/luv2code/springdemo/config/MySpringMvcDispatcherServletInitializer.java
+++ spring-rest-demo/src/main/java/com/luv2code/springdemo/config/MySpringMvcDispatcherServletInitializer.java
@@ -4,18 +4,21 @@ import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatche
 
 public class MySpringMvcDispatcherServletInitializer extends AbstractAnnotationConfigDispatcherServletInitializer {
 
+	// no work for here
 	@Override
 	protected Class<?>[] getRootConfigClasses() {
 		// TODO Auto-generated method stub
 		return null;
 	}
 
+	// 설정 파일 불러오기
 	@Override
 	protected Class<?>[] getServletConfigClasses() {
 		// TODO Auto-generated method stub
 		return new Class[] {DemoAppConfig.class};
 	}
 
+	// root URL 매핑하기
 	@Override
 	protected String[] getServletMappings() {
 		// TODO Auto-generated method stub
