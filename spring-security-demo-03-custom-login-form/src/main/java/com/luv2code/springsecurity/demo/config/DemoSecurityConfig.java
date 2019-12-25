6b743a93d52a13c008815a8dc0958aa37664bdb0
sojeongw <redthink90@gmail.com>
2019-12-25 11:17:50 +0900

    Spring Security - Adding Custom Login Form

--- spring-security-demo-03-custom-login-form/src/main/java/com/luv2code/springsecurity/demo/config/DemoSecurityConfig.java
+++ spring-security-demo-03-custom-login-form/src/main/java/com/luv2code/springsecurity/demo/config/DemoSecurityConfig.java
@@ -2,6 +2,7 @@ package com.luv2code.springsecurity.demo.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.User;
@@ -22,5 +23,16 @@ public class DemoSecurityConfig extends WebSecurityConfigurerAdapter{
 			.withUser(users.username("mary").password("test123").roles("MANAGER"))
 			.withUser(users.username("susan").password("test123").roles("ADMIN"));
 	}
-	
+
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {	// configure security of web paths in application, login, logout etc
+		
+		http.authorizeRequests()
+			.anyRequest().authenticated()
+			.and()
+			.formLogin()
+				.loginPage("/showMyLoginPage")	// show our custom form at the request mapping
+				.loginProcessingUrl("/authenticateTheUser")	// login form should POST data to this URL for processing(check user id and password)
+				.permitAll();	// allow everyone to see login page. no need to be logged in.
+	}
 }
