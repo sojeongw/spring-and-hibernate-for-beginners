9d5a462157918c2ec55bd9c955db7dde713f7bdf
sojeongw <redthink90@gmail.com>
2019-12-27 19:54:46 +0900

    Spring REST - CRUD Database Real-Time Project - Update Customer

--- spring-crm-rest-demo/src/main/java/com/luv2code/springdemo/rest/CustomerRestController.java
+++ spring-crm-rest-demo/src/main/java/com/luv2code/springdemo/rest/CustomerRestController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -51,6 +52,15 @@ public class CustomerRestController {
 		// this is force a save of new item...instead of update
 		// DAO는 대상이 empty일때 create, 아닐 때 update를 하는데 0이나 null을 넣으면 empty라고 인식한다.
 		theCustomer.setId(0);
+		
+		customerService.saveCustomer(theCustomer);
+		
+		return theCustomer;
+	}
+	
+	// add mapping for PUT /customers - update existing customer
+	@PutMapping("/customers")
+	public Customer updateCustomer(@RequestBody Customer theCustomer) {
 		
 		customerService.saveCustomer(theCustomer);
 		
